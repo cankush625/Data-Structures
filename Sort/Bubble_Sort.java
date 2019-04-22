0104e10404817d958b49abebe4ffd92caea817b6
cankush625 <cankush625@gmail.com>
2019-04-22 21:34:28 +0530

    Bubble Sort and Quick Sort Algorithms added

---
+++ Sort/Bubble_Sort.java
@@ -0,0 +1,31 @@
+public class Bubble_Sort{
+    public static void bubble_sort(int[] array1){
+//        boolean isSorted = false;
+//        while(!isSorted) {
+//            isSorted = true;
+            for (int i = 0; i < array1.length - 1; i++) {
+                if (array1[i] > array1[i + 1]) {
+                    swap(array1, i, i + 1);
+//                    isSorted = false;
+//                }
+            }
+        }
+        System.out.println(array1);
+    }
+
+    public static void swap(int[] array1, int i, int j){
+        int temp = array1[i];
+        array1[i] = array1[j];
+        array1[j] = temp;
+    }
+
+    public static void main(String[] args){
+        int[] array1 = {2, 5, 3, 9, 7};
+        bubble_sort(array1);
+    }
+}
+
+/*
+Output :
+[I@73035e27
+*/
---
+++ Sort/Quick_Sort.java
@@ -0,0 +1,53 @@
+public class Quick_Sort {
+//    public static void quickSort(int[] array){
+//        quickSort(array, 0, array.length - 1);
+//    }
+
+    public static void quickSort(int[]  array, int left, int right){
+        if(left <= right){
+            return;
+        }
+
+        int pivot = (left + right)/2;
+        int index = partition(array, left, right, pivot);
+        quickSort(array, left, index - 1 );
+        quickSort(array, index, right);
+    }
+
+    public static int partition(int[] array, int left, int right, int pivot){
+        while (left <= right){
+            while(array[left] < pivot){
+                left++;
+            }
+
+            while (array[right] > pivot){
+                right--;
+            }
+
+            if (left <= right){
+                swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    public static void swap(int[] array, int left, int right){
+        int temp;
+        temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+
+    public static void main(String[] args){
+        int[] array = {2, 5, 3, 9, 7};
+        quickSort(array, 0, array.length - 1);
+        System.out.println(array);
+    }
+}
+
+/*
+    Output :
+        [I@13b6d03
+*/
